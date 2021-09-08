edd0ed9c6556f86394c944adf43b579d09f9ee41
Matt Mores <79879179+MattMores@users.noreply.github.com>
2021-09-08 13:46:00 -0500

    Two Sum Solved

--- Easy/TwoSumAlgo.js
+++ Easy/TwoSumAlgo.js
@@ -5,13 +5,16 @@ MUST be added to eachother. Will be at most one pair of numbers summing
 up to the target */
 
 function twoNumberSum(array, targetSum) {
-    let pointer = 1;
-    let res = [];
-    for (nums of array) {
-        if (nums + array[pointer] !== targetSum) {
-            pointer ++
+    for (let i = 0; i < array.length; i++) {
+        const firstNum = array[i];
+        for (let j = i + 1; j < array.length; j++) {
+            const secondNum = array[j];
+            if (firstNum + secondNum === targetSum) {
+                return [firstNum, secondNum];
+            }
         }
     }
+    return []
 }
 
 console.log(twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10)) // [-1,11]
