280211e629a24712c2dbddd14c80ddea579f3978
Matt Mores <79879179+MattMores@users.noreply.github.com>
2021-09-10 21:54:59 -0500

    sorted squares alternate

--- Easy/SortedSquaredArray.js
+++ Easy/SortedSquaredArray.js
@@ -2,23 +2,43 @@
 //new array of same length with the squares of the original integers
 //also sorted in ascending order.
 
-function sortedSquaredArray(array) {
-    let newArray = []
-    for (let nums of array) {
-        newArray.push(Math.pow(nums, 2))
-    }
-    return newArray.sort((a, b) => a - b);
-  }
+// function sortedSquaredArray(array) {
+//     let newArray = []
+//     for (let nums of array) {
+//         newArray.push(Math.pow(nums, 2))
+//     }
+//     return newArray.sort((a, b) => a - b);
+//   }
 
-  var sortedSquares = function(nums) {
-    let squaredSquares = []
-    for (let i = 0; i < nums.length; i++){
-        num = nums[i]
-        num = Math.pow(num, 2)
-            squaredSquares.push(num)
-    }
-    return squaredSquares.sort((a, b) => a - b)
-};
+//   var sortedSquares = function(nums) {
+//     let squaredSquares = []
+//     for (let i = 0; i < nums.length; i++){
+//         num = nums[i]
+//         num = Math.pow(num, 2)
+//             squaredSquares.push(num)
+//     }
+//     return squaredSquares.sort((a, b) => a - b)
+// };
+
+function sortedSquaredArray(array) {
+    const sortedSquares = [];
+      let smallerValueIdx = 0;
+      let largerValueIdx = array.length -1;
+
+      for (let idx = array.length -1; idx >= 0; idx --) {
+          const smallerValue = array[smallerValueIdx];
+          const largerValue = array[largerValueIdx];
+
+          if (Math.abs(smallerValue) < Math.abs(largerValue)) {
+            sortedSquares.push(smallerValue * smallerValue);
+            smallerValueIdx++;
+          } else {
+            sortedSquares.push(largerValue * largerValue);
+            largerValueIdx--;
+          }
+      }
+    return sortedSquares;
+  }
 
 
 console.log(sortedSquaredArray([1, 2, 3, 5, 6, 8, 9])) // [1, 4, 9, 25, 36, 64, 81]
