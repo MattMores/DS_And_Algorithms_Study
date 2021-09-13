21be5fa88c8d876c93e3184cad71ea7846cea130
Matt Mores <79879179+MattMores@users.noreply.github.com>
2021-09-13 15:24:06 -0500

    newRunning Sum tree

---
+++ Easy/BinarySums.js
@@ -0,0 +1,29 @@
+//Write a funciton that take a Binary Tree and returns a list of its
+//branch sums ordered from leftmost branch sum to rightmost.
+
+class BinaryTree {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function branchSums(root) {
+    const sums = [];
+      traverseTree(root, 0, sums)
+      return sums;
+  }
+
+  function traverseTree (node, runningSum, sums){
+      if (!node) return;
+      const newRunningSum = runningSum + node.value;
+
+      if (!node.left && !node.right) {
+          sums.push(newRunningSum);
+          return
+      }
+
+      traverseTree(node.left, newRunningSum, sums);
+      traverseTree(node.right, newRunningSum, sums);
+  }
