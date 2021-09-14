b4a78c82eafcbb680fae97937e134a4827c49efe
Matt Mores <79879179+MattMores@users.noreply.github.com>
2021-09-14 17:33:37 -0500

    DFS repeat

--- Easy/DepthFirstSearch.js
+++ Easy/DepthFirstSearch.js
@@ -23,3 +23,43 @@ class Node {
         return array;
     }
 }
+
+class Node {
+    constructor(name) {
+        this.name = name;
+        this.children = [];
+    }
+
+    addChild(name) {
+        this.children.push(new Node(name));
+        return this
+    }
+
+    depthFirstSearch(array) {
+        array.push(this.name);
+        for (const child of this.children) {
+            child.depthFirstSearch(array)
+        }
+        return array;
+    }
+}
+
+class Node {
+    constructor(name){
+        this.name = name;
+        this.children = [];
+    }
+
+    addChild(name){
+        this.children.push(new Node(name));
+        return this
+    }
+
+    depthFirstSearch(array){
+        array.push(this.name);
+        for (const child of this.children) {
+            child.depthFirstSearch(array)
+        }
+        return array; 
+    }
+}
