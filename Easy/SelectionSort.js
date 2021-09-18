92d4c113189871259fa750cbf3e2fe3eb1e64beb
Matt Mores <79879179+MattMores@users.noreply.github.com>
2021-09-18 10:59:00 -0500

    pointer swap

---
+++ Easy/SelectionSort.js
@@ -0,0 +1,17 @@
+function selectionSort(array){
+    let startIdx = 0;
+    while(startIdx < array.length -1){
+        let smallestIdx = startIdx;
+        for (let i = startIdx)
+    }
+    for (let i = 0; i < array.length; i++){
+        let number = array[i]
+        let pointer = 1;
+        while(number > array[pointer]){
+            pointer +=
+            swap
+        }
+    }
+}
+
+function swap(array, )
