3b17cc66d8adc7494d0ba64c065eeb65f1fcdc7f
Matt Mores <79879179+MattMores@users.noreply.github.com>
2021-09-20 16:45:10 -0500

    selection sort complete

--- Easy/SelectionSort.js
+++ Easy/SelectionSort.js
@@ -1,17 +1,18 @@
-function selectionSort(array){
+function selectionSort(array) {
     let startIdx = 0;
-    while(startIdx < array.length -1){
+    while (startIdx < array.length -1) {
         let smallestIdx = startIdx;
-        for (let i = startIdx)
-    }
-    for (let i = 0; i < array.length; i++){
-        let number = array[i]
-        let pointer = 1;
-        while(number > array[pointer]){
-            pointer +=
-            swap
+        for (let i = startIdx + 1; i< array.length; i++){
+            if (array[smallestIdx] > array[i]) smallestIdx = i;
         }
+        swap(startIdx, smallestIdx, array);
+        startIdx++;
     }
+    return array;
 }
 
-function swap(array, )
+function swap (i, j, array){
+    const temp = array[j];
+    array[j] = array[i];
+    array[i] = temp
+}
