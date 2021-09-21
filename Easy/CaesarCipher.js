5554d7d00784d66abef5ae82a975b42a27b726d7
Matt Mores <79879179+MattMores@users.noreply.github.com>
2021-09-21 16:16:16 -0500

    caesar

---
+++ Easy/CaesarCipher.js
@@ -0,0 +1,28 @@
+function caesarCipherEncryptor(string, key){
+    const newLetters = [];
+    const newKey = key % 26;
+    for (const letter of string){
+        newLetters.push(getNewLetter(letter, newKey));
+    }
+    return newLetters.join('');
+}
+
+function getNewLetter(letter, key){
+    const newLetterCode = letter.charCodeAt() + key;
+    return newLetter <= 122 ? String.fromCharCode(newLetterCode) : String.fromCharCode(96 + (newLetterCode % 122));
+}
+
+function caesarCipherEncryptor(string, key){
+    const newLetters = [];
+    const newKey = key % 26;
+    const alphabet = 'abcdefghijklmonpqrstuvwxyz'.split('');
+    for (const letter of string) {
+        newLetters.push(getNewLetter(letter, newKey, alphabet));
+    }
+    return newLetters.join('');
+}
+
+function getNewLetter(letter, key){
+    const newLetterCode = letter.charCodeAt() + key;
+    return newLetterCode <= 122 ? String.fromCharCode(newLetterCode) : String.fromCharCode(96 + (newLetterCode % 122));
+}
---
+++ Easy/playground.js
@@ -0,0 +1,8 @@
+let string = "abcdefg"
+let reversed = ""
+for(let i = 0; i <= string.length -1; i++){
+    let word = string[i];
+    reversed += word
+}
+
+console.log(reversed)
