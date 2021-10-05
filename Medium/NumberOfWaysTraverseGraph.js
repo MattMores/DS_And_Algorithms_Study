d0dc6028bc8c68f46cd3b27a42cff2827b349016
Matt Mores <79879179+MattMores@users.noreply.github.com>
2021-10-05 15:44:14 -0500

    cycle

--- Medium/NumberOfWaysTraverseGraph.js
+++ Medium/NumberOfWaysTraverseGraph.js
@@ -16,3 +16,26 @@ function numberOfWaysToTraverseGraph(width, height){
 
     return numberOfWaysToTraverseGraph(width - 1, height) + numberOfWaysToTraverseGraph(width, height -1)
 }
+
+function numberOfWaysToTraverseGraph(width, height){
+    const numberOfWays = [];
+    for (let i = 0; i < height + 1; i++){
+        numberOfWays.push([]);
+        for (let j = 0; j < width + 1; j++){
+            numberOfWays[i].push(0);
+        }
+    }
+
+    for (let widthIdx = 1; widthIdx < width + 1; widthIdx++){
+        for (let heightIdx = 1; heightIdx < height + 1; heightIdx++){
+            if (widthIdx === 1 || heightIdx === 1){
+                numberOfWays[heightIdx][widthIdx] = 1;
+            } else {
+                const waysLeft = numberOfWays[heightIdx][widthIdx - 1];
+                const waysUp = numberOfWays[heightIdx - 1][widthIdx];
+                numberOfWays[heightIdx][widthIdx] = waysLeft + waysUp;
+            }
+        }
+    }
+    return numberOfWays[height][width]
+}
