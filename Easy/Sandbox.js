f7ecd8f9156228eb09bc6c6dde729773b448c0db
Matt Mores <79879179+MattMores@users.noreply.github.com>
2021-10-19 18:08:50 -0500

    alternates

--- Easy/Sandbox.js
+++ Easy/Sandbox.js
@@ -1,12 +1,27 @@
-let twoNumberSum = function (array, targetSum){
-    let map = new Map();
-    for (const nums of array){
-        const potentialMatch = targetSum - nums;
-        if (map.has(potentialMatch)){
-            return [potentialMatch, nums]
-        } else {
-            map.set(nums, true)
-        }
+function minimumWaitingTime(queries){
+    let sorted = queries.sort((a, b) => a - b)
+    let waitingTime = 0; // 1
+    let totalWaitingTime = 0; // 0
+
+    for (const duration of queries){
+        totalWaitingTime += waitingTime;
+        waitingTime += duration;
     }
-    return [];
+    return totalWaitingTime;
 }
+
+function minimumWaitingTime(queries){
+    queries.sort((a, b) => a - b);
+
+    let totalWaitingTime = 0;
+
+    for (let i = 0; i < queries.length; i++){
+        const duration = queries[i];
+        const queriesLeft = queries.length - (i + 1);
+        totalWaitingTIme += duration * queriesLeft
+    }
+    return totalWaitingTIme
+}
+
+
+console.log(minimumWaitingTime([3, 2, 1, 2, 6]))
