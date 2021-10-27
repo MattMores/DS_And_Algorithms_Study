8405ac597b57e3a84cc4cbbbd8ce62102bb8911d
Matt Mores <79879179+MattMores@users.noreply.github.com>
2021-10-27 14:55:14 -0500

    10.27 push

--- Easy/Sandbox.js
+++ Easy/Sandbox.js
@@ -1,56 +1,17 @@
 function classPhotos(redShirtHeights, blueShirtHeights){
-    redShirtHeights.sort((a, b) => b - a);
-    blueShirtHeights.sort((a, b) => b - a);
+    const redSorted = redShirtHeights.sort((a, b) => a - b);
+    const blueSorted = blueShirtHeights.sort((a, b) => a - b);
 
-    const shirtColorInFirstRow = redShirtHeights[0] < blueShirtHeights[0] ? "RED" : "BLUE";
-    for (let i = 0; i < redShirtHeights.length; i++){
-        const redShirtHeight = redShirtHeight[i];
-        const blueShirtHeight = blueShirtHeights[i];
-        if (shirtColorInFirstRow === "RED"){
-            if (redShirtHeight >= blueShirtHeight) return false;
-        } else if (blueShirtHeight >= redShirtHeight) return false;
+    let frontRow = redSorted[0] < blueSorted[0] ? "RED" : "BLUE";
+
+    if (frontRow === "RED") {
+        for (const i = 0; i < redSorted.length; i++) {
+            if (redSorted[i] >= blueSorted[i]) return false
+        }
+    else if (frontRow === "BLUE"){
+        for (const i = 0; i < blueSorted.length; i++){
+            if (blueSorted[i] >= redSorted[i]) return false
+        }
     }
-    return true;
-}
-
-
-function classPhotos(redShirtHeights, blueShirtHeights){
-    redShirtHeights.sort((a, b) => b - a);
-    blueShirtHeights.sort((a, b) => b - a);
-
-    const shirtColorInFirstRow = redShirtHeights[0] < blueShirtHeights[0] ? "RED" : "BLUE"
-    for (let i = 0; i < redShirtHeights.length; i++){
-        const redShirtHeight = redShirtHeights[i];
-        const blueShirtHeight = blueShirtHeights[i];
-        if(shirtColorInFirstRow === 'RED'){
-            if (redShirtHeight >= blueShirtHeight) return false;
-        } else if (blueShirtHeight >= redShirtHeight) return false;
-    }
-    return true;
-}
-
-function minimumWaitingTime(querires){
-    querires.sort((a, b) => a - b);
-
-    let waitingTime = 0;
-    let totalWaitingTime = 0;
-
-    for (const duration of queries){
-        totalWaitingTime += waitingTIme;
-        waitingTime += duration;
-    }
-    return totalWaitingTime;
-}
-
-funciton minimumWaitingTime(querires){
-    queries.sort((a,b) => a - b);
-
-    let totalWaitingTime = 0;
-    let waitingTime = 0;
-    for (const duration of querires){
-        totalWaitingTime += waitingTime;
-        waitingTime += duration;
-    }
-
-    return totalWaitingTime;
+    return true
 }
